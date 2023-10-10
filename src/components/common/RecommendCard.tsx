cb9685b96f82060e35b8a089d28b2799170fe1c9
zeqingwang <61963896+zeqingwang@users.noreply.github.com>
2023-10-09 21:17:56 -0700

    change css to tailwindcss

--- src/components/common/RecommendCard.tsx
+++ src/components/common/RecommendCard.tsx
@@ -10,13 +10,13 @@ interface RecommendCardProps {
 
 const RecommendCard: React.FC<RecommendCardProps> = ({ imageUrl, title, category, originalPrice, discountedPrice }) => {
     return (
-        <div className="recommend-card">
-            <img src={imageUrl} alt={title} />
-            <div className="title">{title}</div>
-            <div className="category">{category}</div>
-            <div className="prices">
-                <span className="original-price">${originalPrice}</span>
-                <span className="discounted-price">${discountedPrice}</span>
+        <div className="border border-gray-300 rounded-lg w-183 h-324">
+            <img src={imageUrl} alt={title} className="w-full h-auto" />
+            <div className="mt-10 text-center text-gray-900 font-bold text-xl leading-6 tracking-wider">{title}</div>
+            <div className="mt-2.5 text-center text-gray-600 font-bold text-base leading-6 tracking-wide">{category}</div>
+            <div className="mt-2.5 text-center">
+                <span className="text-gray-400 font-bold text-xl leading-6 tracking-wider mr-1.5">${originalPrice}</span>
+                <span className="text-secondary-1 font-bold text-xl leading-6 tracking-wider">${discountedPrice}</span>
             </div>
         </div>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,38 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'gray-400': '#BDBDBD',
+        'secondary-1': '#23856D',
+        'gray-900': '#252B42',
+        'gray-600': '#737373',
+        'gray-300': '#ECECEC',
+      },
+      width: {
+        '183': '183px',
+        '658': '658px',
+        '113': '113px',
+        '48': '48px',
+        '389': '389px',
+      },
+      height: {
+        '324': '324px',
+        '48': '48px',
+        '2': '2px',
+      },
+      spacing: {
+        '30': '30px',
+        '10': '10px',
+        '15': '15px',
+        '24': '24px',
+        '48': '48px',
+      },
+      fontFamily: {
+        'sans': ['Montserrat', 'sans-serif'],
+      }
+
+    },
   },
   plugins: [],
 }
